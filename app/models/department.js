c7a99607dda928dd11a1e0e3cd3e417542aff6a5
Simon Tseng <simon@simontseng.com>
2016-06-07 02:39:31 +0800

    fixed import and reference errors

--- app/models/department.js
+++ app/models/department.js
@@ -1,7 +1,6 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
+import DS from 'ember-data';
 
-export default Model.extend({
-  targets: hasMany(),
-  name: attr()
+export default DS.Model.extend({
+  targets: DS.hasMany(),
+  name: DS.attr()
 });
